0aeefb1f4e4d08cb6aefb61ec0032d129174a973
MinKhantSoeOke <oke@student.agh.edu.pl>
2025-02-15 22:20:18 +0100

    Update App.jsx with code formatting improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
